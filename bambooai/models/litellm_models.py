1036148279ba771acbe5f8ff39904b77cfe4e338
Ranu Yulianto <rn.yulianto@gmail.com>
2025-09-23 16:13:33 +0700

    Fix error in litellm model

--- bambooai/models/litellm_models.py
+++ bambooai/models/litellm_models.py
@@ -37,12 +37,12 @@ def llm_call(messages: str,model: str,temperature: str,max_tokens: str, response
 
     try:
         start_time = time.time()
-        response = get_response(provider, model, messages, temperature, max_tokens, response_format)
+        response = get_response(model, provider, messages, temperature, max_tokens, response_format)
         end_time = time.time()
     except RateLimitError:
         time.sleep(10)
         start_time = time.time()
-        response = get_response(provider, model, messages, temperature, max_tokens, response_format)
+        response = get_response(model, provider, messages, temperature, max_tokens, response_format)
         end_time = time.time()
 
     elapsed_time = end_time - start_time
@@ -87,7 +87,8 @@ def llm_stream(prompt_manager, log_and_call_manager, output_manager, chain_id: s
         }
         search_triplets.append(triplet)
 
-    def get_response(provider, model, messages, temperature, max_tokens, tools, response_format, reasoning_models=None, reasoning_effort="low"):
+    def get_response(model, provider, messages, temperature, max_tokens, tools, response_format, reasoning_models=None, reasoning_effort="low"):
+        print(f"Using model: {model} from provider: {provider}")
         if reasoning_models and model in reasoning_models:
             output_manager.display_tool_info('Thinking', f"Reasoning Effort: {reasoning_effort}", chain_id=chain_id)
             return completion(
@@ -118,7 +119,7 @@ def llm_stream(prompt_manager, log_and_call_manager, output_manager, chain_id: s
         combined_completion_tokens_used = 0
         combined_total_tokens_used = 0
 
-        response = get_response(provider, model, messages, temperature, max_tokens, tools, response_format, reasoning_models, reasoning_effort)
+        response = get_response(model, provider, messages, temperature, max_tokens, tools, response_format, reasoning_models, reasoning_effort)
 
         start_time = time.time()
         # iterate through the stream of events
@@ -194,7 +195,7 @@ def llm_stream(prompt_manager, log_and_call_manager, output_manager, chain_id: s
 
             # Check if it's the last tool call in the list before calling 
             if index == len(tool_calls) - 1:
-                response = get_response(model, messages, temperature, max_tokens, tools, response_format, reasoning_models, reasoning_effort)
+                response = get_response(model, provider, messages, temperature, max_tokens, tools, response_format, reasoning_models, reasoning_effort)
 
         # iterate through the stream of events
         for chunk in response:
